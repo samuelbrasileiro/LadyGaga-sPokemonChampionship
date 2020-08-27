7e7d91b9e6cfaeb542b64228d3af688254f3e63e
Samuel <samuelbsantosn@gmail.com>
2020-08-27 14:17:43 -0300

    Adicionando menu Gameboy e imagens

--- Lady Gaga's Pokemon Championship/MenuViewController.swift
+++ Lady Gaga's Pokemon Championship/MenuViewController.swift
@@ -8,19 +8,44 @@
 
 import UIKit
 import AVKit
+
+
 class MenuViewController: UIViewController {
     var player: AVPlayer?
     
-    @IBOutlet var startButton: UIButton!
+    @IBOutlet var AButton: UIButton!
+    
+    @IBOutlet var startLabel: UILabel!
+    
+    @IBOutlet var attentionLabel: UILabel!
+    
+    @IBOutlet var challengeImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         startSong()
         
-        UIButton.animate(withDuration: 1.5, delay: 0.0, options: [.repeat, .autoreverse], animations: {
-            self.startButton.transform = CGAffineTransform(scaleX: 1.08, y: 1.08)
-            self.startButton.clipsToBounds = true;
+        challengeImage.isHidden = true
+        
+        self.startLabel.alpha = 0.5
+        self.attentionLabel.alpha = 0
+        self.attentionLabel.text = ""
+        UILabel.animate(withDuration: 1.5, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+            self.startLabel.transform = CGAffineTransform(scaleX: 1.08, y: 1.08)
+            self.startLabel.alpha = 1
         })
+        
+        UILabel.animate(withDuration: 0.3, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+            self.attentionLabel.transform = CGAffineTransform(scaleX: 1.08, y: 1.08)
+            self.attentionLabel.text = "..."
+            self.attentionLabel.alpha = 1
+        })
+        
+        
+        Timer.scheduledTimer(withTimeInterval: 4, repeats: false) {_ in
+            self.attentionLabel.isHidden = true
+            self.challengeImage.isHidden = false
+        }
         
     }
     
@@ -39,7 +64,7 @@ class MenuViewController: UIViewController {
         }
     }
     
-    @IBAction func startButtonAction(_ sender: Any) {
+    @IBAction func AButtonAction(_ sender: Any) {
         print("alo")
         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "Recognition")
         navigationController?.pushViewController(vc, animated: true)
